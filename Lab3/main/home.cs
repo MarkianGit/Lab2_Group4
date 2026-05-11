658e1496b52030f6792dee1416af1ce15ede16f4
MarkianGit <markian0204@gmail.com>
2026-05-11 22:06:39 +0300

    1

---
+++ Lab3/main/home.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TravelPlanner.Models;
+
+namespace Lab3.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trip trip = new Trip();
+
+            trip.Name = "Літня подорож";
+            trip.Country = "Італія";
+            trip.StartDate = new DateTime(2026, 7, 10);
+            trip.EndDate = new DateTime(2026, 7, 20);
+            trip.Budget = 50000;
+
+            trip.ShowInfo();
+
+            Expense expense1 = new Expense("Готель", 15000);
+            Expense expense2 = new Expense("Квитки", 12000);
+
+            Console.WriteLine();
+            Console.WriteLine("Витрати:");
+            Console.WriteLine($"{expense1.Category}: {expense1.Amount} грн");
+            Console.WriteLine($"{expense2.Category}: {expense2.Amount} грн");
+
+            PackingItem item = new PackingItem("Паспорт");
+            item.PackItem();
+
+            Console.WriteLine();
+            Console.WriteLine($"Річ: {item.ItemName}");
+            Console.WriteLine($"Зібрано: {item.IsPacked}");
+        }
+    }
+}
